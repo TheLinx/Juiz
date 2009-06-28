23d72cf4a3a324175b2f22f5f0fdcfc00fe3fc81
Linus Sjögren <thelinx@unreliablepollution.net>
2009-06-28 18:24:50 +0000

    Added module HTTP Status by TheLinx

---
+++ modules/httpstatus.lua
@@ -0,0 +1,9 @@
+local http = require("socket.http")
+function cmd_httpstatus(recp, sender, url)
+    local _,_,_,status = http.request(url)
+    say(string.format("%s: HTTP Status of %s is %s", sender, url, status))
+    return true
+end
+
+ccmd.Add("httpstatus", cmd_httpstatus)
+msg("INSTALL", "Loaded HTTP Status (http://code.google.com/p/juiz/wiki/httpstatus)")

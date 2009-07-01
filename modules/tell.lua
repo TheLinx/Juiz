8431dfac83838af0487fec6c3bc2c6d945011c65
Linus Sjögren <thelinx@unreliablepollution.net>
2009-07-01 13:37:45 +0000

    Added some more recipient checking to the Tell module and changed the messages a bit.
    -- Detailed changelog:
    Tell Module:
      * You can no longer use the tell command without supplying an argument.
      * You can no longer leave a message to an empty recipient.
      * You can no longer leave empty messages.
      * Now replies "Okay, I'll tell %s that when he/she is back." to the user leaving a message.
      * Now replies "You can't do that." if Juiz encounters an error.

--- modules/tell.lua
+++ modules/tell.lua
@@ -1,13 +1,17 @@
 local telldb = {}
 local function cmd_tell(recp, sender, message)
+    if message == '' or message == nil then
+        reply(recp, sender, "You can't do that.")
+        return true
+    end
     messageto = string.sub(message, 0, message:find(" ")-1)
     messagetext = string.sub(message, message:find(" ")+1)
-    if messageto:lower() == config.nick:lower() or sender:lower() == messageto:lower() then
-        reply(recp, sender, "I'm not that stupid")
+    if messageto:lower() == config.nick:lower() or sender:lower() == messageto:lower() or messageto == '' or messagetext == '' then
+        reply(recp, sender, "You can't do that.")
         return true
     end
     msg("TRACE", string.format("%s left a message to %s: %s", sender, messageto, messagetext))
-    reply(recp, sender, "Okay, I'll tell him that when I see him")
+    reply(recp, sender, string.format("Okay, I'll tell %s that when he/she is back.", messageto))
     table.insert(telldb, {messageto, messagetext, sender})
     return true
 end

f0105f407643f869423deabe1bc224783251fa51
Linus Sjögren <thelinx@unreliablepollution.net>
2009-09-10 18:55:45 +0200

    The seen reply is now more detailed; contains act of user

--- modules/seen.lua
+++ modules/seen.lua
@@ -15,18 +15,29 @@ juiz.addccmd("seen", {function (recp, sender, user)
         return true
     end
     local seendata = juiz.getdata("seendb-"..user)
-    if seendata then
-        juiz.reply(recp, sender, "I last saw %s around %s.", user, os.date("%c", seendata))
+    if type(seendata) == "table" then
+        local seentime,seenact,seenloc = os.date("%c", seendata[1]),seendata[2],seendata[3]
+        if not seenloc:find("#") then seenloc = "a private chat" end
+        if seenact == "j" then
+            return juiz.reply(recp, sender, "I last saw %s join %s around %s.", user, seenloc, seentime)
+        elseif seenact == "p" then
+            return juiz.reply(recp, sender, "I last saw %s leave %s around %s.", user, seenloc, seentime)
+        elseif seenact == "m" then
+            return juiz.reply(recp, sender, "I last saw %s say something in %s around %s.", user, seenloc, seentime)
+        end
+    elseif seendata then
+        return juiz.reply(recp, sender, "I last saw %s around %s.", user, os.date("%c", seendata))
     else
-        juiz.reply(recp, sender, "Sorry, I haven't seen %s around.", user)
+        return juiz.reply(recp, sender, "Sorry, I haven't seen %s around.", user)
     end
     return true
 end, "<user>", "reports the last occasion that the bot saw the user."})
-function userseen(sender, recp)
+function userseen(hook, sender, recp)
     if sender:lower() == config.nick:lower() then return end
-    return juiz.setdata("seendb-"..sender, os.time())
+    return juiz.setdata("seendb-"..sender, {os.time(), hook, recp})
 end
 
-juiz.addhook("message", userseen)
-juiz.addhook("join", userseen)
-juiz.registermodule("seen", "Last Seen Command", 1)
+juiz.addhook("message", function (...) userseen("m", ...) end)
+juiz.addhook("join", function (...) userseen("j", ...) end)
+juiz.addhook("part", function (...) userseen("p", ...) end)
+juiz.registermodule("seen", "Last Seen Command", 2)

9405455f2deff1b4e4c1862a8864f9450d4b8882
Linus Sjögren <thelinx@unreliablepollution.net>
2009-09-12 15:39:33 +0200

    data: Now uses JSON for saving. This allows nested tables in the datatable.

--- modules/data.lua
+++ modules/data.lua
@@ -3,8 +3,10 @@
 --- Made by: Linus Sjögren (thelinx@unreliablepollution.net)
 --- Depends on:
 ---  * Utility functions (any version)
+---  * (External) JSON4Lua - http://json.luaforge.net/#download
 --- License: MIT
 ---------------------------------------------------------------------
+require("json")
 juiz.depcheck({"util"},{1})
 data,datatable = {},{}
 
@@ -30,12 +32,9 @@ end
 -- @param save Specify "false" if you're going to do a lot of data adding, then save manually.
 function juiz.adddata(id, adata, save)
     if type(adata) ~= "table" then adata = {adata} end
-    local existingdata = juiz.getdata(id)
-    if existingdata ~= nil then
-        util.msg("TRACE", "Data for category %s already there!", id)
-        for k,v in pairs(existingdata) do print(k,v) adata[#adata + 1] = v end
-    end
-    juiz.setdata(id, adata)
+    existingdata = juiz.getdata(id) or {}
+    table.insert(existingdata, adata)
+    juiz.setdata(id, existingdata)
     if save ~= false then juiz.savedata() end
 end
 
@@ -50,22 +49,9 @@ end
 --- Saves data.
 function juiz.savedata()
     util.msg("TRACE", "Saving data...")
-    local lastcat,fcon = '','---JUIZ IRC BOT SAVED DATA---'
-    local tbl = datatable
-    for k,v in pairs(datatable) do
-        if lastcat ~= k then
-            util.msg("TRACE", "Changing category to %s", k)
-            fcon = string.format("%s\n[%s]", fcon, k)
-            lastcat = k
-        end
-        if type(v) == "table" then
-            fcon = string.format("%s\n%s", fcon, table.concat(v, "|"))
-        else
-            fcon = string.format("%s\n%s", fcon, v)
-        end
-    end
+    fcon = json.encode(datatable)
     util.msg("TRACE", "Generated this data: %s", fcon)
-    local fopn = io.open("saved.txt", "w")
+    local fopn = io.open("saved", "w")
     fopn:write(fcon)
     fopn:close()
     util.msg("NOTIFY", "Saved data.")
@@ -73,24 +59,12 @@ function juiz.savedata()
 end
 
 -- Load the saved data on load
-local fopn = io.open("saved.txt", "r")
+local fopn = io.open("saved", "r")
 if fopn then
-    local i,cat = 1,'nil'
-    for line in fopn:lines() do
-        util.msg("TRACE", "read saved line %s", line)
-        if i == 1 and line == '---JUIZ IRC BOT SAVED DATA---' then
-            -- found saved data
-        elseif string.sub(line, 1, 1) == "[" and string.sub(line, line:len(), line:len()) == "]" then
-            util.msg("TRACE", "category changed to %s", string.sub(line, 2, line:len()-1))
-            cat = string.sub(line, 2, line:len()-1)
-        else
-            if string.find(line, "|") then
-                line = util.explode("|", line)
-            end
-            juiz.setdata(cat, line, false)
-        end
-        i = i + 1
+    local fcon = fopn:read("*all")
+    if fcon:len() > 1 then
+        datatable = json.decode(fcon)
     end
 end
 
-juiz.registermodule("data", "Data Saving", 2)
+juiz.registermodule("data", "Data Saving", 3)
--- modules/tell.lua
+++ modules/tell.lua
@@ -7,39 +7,34 @@
 ---  * Utility functions (any version)
 --- License: MIT
 ---------------------------------------------------------------------
-juiz.depcheck({"util","ccmd","data"},{1,1,2})
+juiz.depcheck({"util","ccmd","data"},{1,1,3})
 
 juiz.addccmd("tell", {function (recp, sender, message)
     if message == '' or message == nil or not message:find(" ") then
-        juiz.reply(recp, sender, "You can't do that.")
-        return true
+        return juiz.reply(recp, sender, "You can't do that.")
     end
     messageto = string.sub(message, 0, message:find(" ")-1)
     messagetext = string.sub(message, message:find(" ")+1)
     if messageto:lower() == config.nick:lower() or sender:lower() == messageto:lower() or messageto == '' or messagetext == '' then
-        juiz.reply(recp, sender, "You can't do that.")
-        return true
+        return juiz.reply(recp, sender, "You can't do that.")
     end
     util.msg("TRACE", "%s left a message to %s: %s", sender, messageto, messagetext)
     juiz.reply(recp, sender, "Okay, I'll tell %s that when he/she is back.", messageto)
-    juiz.adddata("telldb-"..messageto, string.format("%s %s", sender, messagetext))
+    juiz.adddata("telldb-"..messageto:lower(), {sender, messagetext})
     return true
 end, "<user> <message>", "takes a message for another user, then tells them when they come back."})
 function usercheck(sender, recp)
     if sender:lower() == config.nick:lower() then return end
     util.msg("TRACE", "Checking if %s has any messages...", sender)
-    local telldb = juiz.getdata("telldb-"..sender)
+    local telldb = juiz.getdata("telldb-"..sender:lower())
     if telldb == nil then return true
-    elseif type(telldb) == "table" then
+    else
         for _,v in pairs(telldb) do
-            local messagefrom,messagetext = v:match("^(%S+) (.*)")
+            local messagefrom,messagetext = v[1],v[2]
             juiz.reply(recp, sender, "%s left this message to you: '%s'", messagefrom, tostring(messagetext))
         end
-    elseif type(telldb) == "string" then
-        local messagefrom,messagetext = telldb:match("^(%S+) (.*)")
-        juiz.reply(recp, sender, "%s left this message to you: '%s'", messagefrom, tostring(messagetext))
     end
-    juiz.removedata("telldb-"..sender)
+    juiz.removedata("telldb-"..sender:lower())
 end
 
 juiz.addhook("message", usercheck)

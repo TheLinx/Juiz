dc3efc254eb4a45fd80853fd01b65caad94e69b2
Linus Sjögren <thelinx@unreliablepollution.net>
2009-09-13 12:51:12 +0200

    What time is it? Juiz has the answer.

---
+++ modules/thetime.lua
@@ -0,0 +1,12 @@
+---------------------------------------------------------------------
+--- The time
+--- Made by: Linus Sjögren (thelinx@unreliablepollution.net)
+--- License: Public Domain
+---------------------------------------------------------------------
+juiz.addhook("message", function (sender, recp, message)
+    if not message:find("what") then return end
+    if not message:find("time") then return end
+    return juiz.reply(recp, sender, "The time is %s", os.date("%c"))
+end)
+
+juiz.registermodule("thetime", "The time", 1)

785b78638aafef665eb315cc5c19ddcb9e0967f0
Linus Sjögren <thelinx@unreliablepollution.net>
2009-10-01 14:29:19 +0200

    removed printr (was for debugging) added depcheck (oops)

--- modules/webfuncs.lua
+++ modules/webfuncs.lua
@@ -6,24 +6,11 @@
 ---  * (External) JSON4Lua - http://json.luaforge.net/#download
 --- License: Public Domain
 ---------------------------------------------------------------------
-require("json")
-http = require("socket.http")
-url = require("socket.url")
+local json = require("json")
+local http = require("socket.http")
+local url = require("socket.url")
+juiz.depcheck({"util"}, {3})
 webfunc = {}
-
-local function printr(it, steps)
-    if not it then return end
-    steps = steps or 0
-    for k,i in pairs(it) do
-        if type(i) == "table" then
-            print (string.rep(" ", steps)..k.." => ".."table:")
-            printr(i, steps + 1)
-        else
-            print(string.rep(" ", steps)..k.." => "..tostring(i))
-        end
-    end
-    return true
-end
 
 local function googlejson(query)
     local c = http.request("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&safe=off&q="..url.escape(query))

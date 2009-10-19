9e9a694fdd8fd9bdb17d1d7a760084ad6b52fcac
Linus Sjögren <thelinx@unreliablepollution.net>
2009-10-19 20:25:53 +0200

    defused mines, added tail calls.

--- modules/webinstall.lua
+++ modules/webinstall.lua
@@ -10,8 +10,9 @@ juiz.depcheck({"util","ccmd"}, {1,1})
 
 --- Downloads and loads a module.
 -- @param file The URI of the file to download.
--- @return boolean true or false depending on success of loadmodule()
-function juiz.webinstall (file)
+-- @return boolean true or false depending on success of juiz.loadmodule()
+function juiz.webinstall(file)
+    if not file:find("/") then return false end
     local fcon,_,h = http.request(file)
     local fnam = file
     while true do
@@ -19,30 +20,24 @@ function juiz.webinstall (file)
         if not i then break end
         fnam = string.sub(s, i+1)
     end
-    local fopn = io.open("modules/"..fnam..".lua", "w+")
+    local fopn = assert(io.open("modules/"..fnam..".lua", "w+"))
     fopn:write(fcon)
     fopn:close()
-    if loadmodule(fnam) then
-        return true
-    else
-        return false
-    end
+    return juiz.loadmodule(fnam)
 end
 
 juiz.addccmd("webinstall", {function (recp, sender, file, host)
     if not juiz.isowner(sender, host) then
-        juiz.reply(recp, sender, "You're not authorized to use that command.")
-        return true
+        return juiz.reply(recp, sender, "You're not authorized to use that command.")
     end
     if not file then
         juiz.reply(recp, sender, "Insufficient arguments supplied!")
     end
     if juiz.webinstall(file) then
-        juiz.reply(recp, sender, "Success!")
+        return juiz.reply(recp, sender, "Success!")
     else
-        juiz.reply(recp, sender, "Installation failed!")
+        return juiz.reply(recp, sender, "Installation failed!")
     end
-    return true
 end, "<url>", "downloads and includes a Lua file. (owner only)"})
 juiz.aliasccmd("winstall", "webinstall")
 

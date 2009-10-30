f72aa8c6fcd54f2831a05f56180835df5117935e
Linus Sjögren <thelinx@unreliablepollution.net>
2009-10-31 00:10:42 +0100

    fixed error

--- modules/gender.lua
+++ modules/gender.lua
@@ -14,8 +14,8 @@ juiz.addccmd("gender", {function (recp, sender, message)
     if message == nil then
         return juiz.reply(recp, sender, "You can't do that.")
     end
-    juiz.setdata("genderdb-"..theuser:lower(), thegender)
-    util.msg("TRACE", "%s changed %s's gender to %s", sender, theuser, thegender)
+    juiz.setdata("genderdb-"..sender:lower(), message)
+    util.msg("TRACE", "%s changed gender to %s", sender, message)
     juiz.reply(recp, sender, "Changed your gender to %s.", message)
     return true
 end, "<gender>", "take note of the gender of you, used for tell."})

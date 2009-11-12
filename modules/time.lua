ba2a0a93744fa56fddd23a6f3f7d251bcc44f2c2
Linus Sjögren <thelinx@unreliablepollution.net>
2009-11-12 15:00:48 +0100

    oops

--- modules/time.lua
+++ modules/time.lua
@@ -6,8 +6,6 @@
 juiz.depcheck({"util","ccmd"},{1,1})
 
 juiz.addccmd("t", {function (recp, sender, message)
-    if not message:find("what") then return end
-    if not message:find("time") then return end
     return juiz.reply(recp, sender, "The time is %s", os.date("%c"))
 end, "", "asks the bot what time it is."})
 
